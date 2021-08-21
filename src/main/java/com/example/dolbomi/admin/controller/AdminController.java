466ae8f9cefeb843957f0280cd046e6e92a49f49
enyung <45346786+eyl056@users.noreply.github.com>
2021-08-21 21:47:03 +0900

    Fix: 관리자 노인 등록 api url 수정

--- src/main/java/com/example/dolbomi/admin/controller/AdminController.java
+++ src/main/java/com/example/dolbomi/admin/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@RequestMapping("/users")
+@RequestMapping("/admins")
 @RequiredArgsConstructor
 @RestController
 public class AdminController {
